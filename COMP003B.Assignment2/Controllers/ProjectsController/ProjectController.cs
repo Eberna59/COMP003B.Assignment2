509636e1ef8ec76682e260bb08f88b3dac3b3f67
evanbern59 <evansantos1202@gmail.com>
2026-03-26 21:33:58 -0700

    made the folder into cs and then fixed it to how it should be and added the contreoller

---
+++ COMP003B.Assignment2/Controllers/ProjectsController/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers.ProjectsController
+{
+    public class ProjectController : Controller
+    {
+        [HttpGet]
+        public IActionResult Current()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Completed()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Ideas()
+        {
+            return View();
+        }
+    }
+}
