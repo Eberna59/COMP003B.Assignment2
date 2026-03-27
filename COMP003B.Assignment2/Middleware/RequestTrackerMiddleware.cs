1070d65959df6bf451960cd442b320a3f7c52aad
evanbern59 <evansantos1202@gmail.com>
2026-03-26 21:22:59 -0700

    requesttrackermiddleware.cs

---
+++ COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace COMP003B.Assignment2.Middleware
+{
+    public class RequestTrackerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestTrackerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+
+            await _next(context);
+
+            Console.WriteLine($"Response Status Code: {context.Response.StatusCode}");
+        }
+    }
+}
